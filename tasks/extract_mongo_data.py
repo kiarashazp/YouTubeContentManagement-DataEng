7eba84d27c7cacc5799d88c100e773bcd7d5c5ff
Rubik0x7b4 <rubik0x7b4@gmail.com>
2025-01-09 15:31:07 +0100

    Add batch processing for MongoDB extraction

--- tasks/extract_mongo_data.py
+++ tasks/extract_mongo_data.py
@@ -7,20 +7,29 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def extract_mongo_data():
+def extract_mongo_data(**kwargs):
     try:
+        # You can adjust the batch size in the Airflow UI without changing the code.
+        batch_size = int(Variable.get("mongo_batch_size", default_var=1000))
         mongo_hook = MongoHook(conn_id=MONGO_CONN_ID)
         collection_name = Variable.get("mongo_collection_name")
         collection = mongo_hook.get_collection(collection_name)
-        data = collection.find()
+        cursor = collection.find().batch_size(batch_size)
         
-        for document in data:
-            logger.info(document)
+        batches = []
+        while True:
+            batch = list(cursor.next_batch())
+            if not batch:
+                break
+            batches.append(batch)
+        
+        # Push the batches to XCom
+        kwargs['ti'].xcom_push(key='mongo_batches', value=batches)
         
     except Exception as error:
         logger.error(f"Error extracting data from MongoDB: {error}")
         raise
 
-
 if __name__ == "__main__":
     extract_mongo_data()
+
---
+++ tasks/schema_creation.py
@@ -0,0 +1,8 @@
+from clickhouse_driver import Client
+
+def create_clickhouse_schema():
+    client = Client('clickhouse_host')
+    client.execute('''
+        
+    ''')
+
