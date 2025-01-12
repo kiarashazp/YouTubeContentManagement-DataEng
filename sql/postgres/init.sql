be78e8cf480ea4f573b360f70db89781747ba0f9
Rational-Pigeon <y.nematzade.74@gmail.com>
2025-01-12 17:14:18 +0330

    postgres update based on repo structure  - modifying docker-compose.yaml to set up postgres container  - movign README.md and init.sql based on the updated project structure  - adding .env for storing db variables.

--- sql/postgres/init.sql
+++ sql/postgres/init.sql
@@ -13,4 +13,3 @@ CREATE TABLE IF NOT EXISTS channels (
 );
 
 \COPY channels (username, total_video_visit, video_count, start_date_timestamp, followers_count, country, created_at, update_count) FROM '/docker-entrypoint-initdb.d/raw-channel-data/channels_stripped.csv' WITH (FORMAT csv, HEADER, DELIMITER ',', QUOTE '"', ESCAPE '"');
-
