0db6a3d137610ab044b374fe3feb4fe16754a72a
kiarash <kiarashazp78@gmail.com>
2025-01-15 23:20:38 +0330

    correct name collection and bson name.

--- mongodb/init_db_mongo.sh
+++ mongodb/init_db_mongo.sh
@@ -4,4 +4,4 @@
 mongod --bind_ip_all &
 
 # Restore the database mongo and insert data in mongodb
-mongorestore drop --host localhost --port 27017 --db videos --collection videos ../data/videos-raw/videos.bson
+mongorestore drop --host localhost --port 27017 --db videos --collection videos ../data/videos-raw/videos/videos.bson
