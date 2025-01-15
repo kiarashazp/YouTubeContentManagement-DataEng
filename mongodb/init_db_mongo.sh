ecf85b40db2e473a2e49a2e3319e8c85d92506ae
kiarash <kiarashazp78@gmail.com>
2025-01-15 23:30:27 +0330

    absolute path for mongorestore

--- mongodb/init_db_mongo.sh
+++ mongodb/init_db_mongo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start MongoDB in the background
-mongod --bind_ip_all &
+# mongod --bind_ip_all &
 
 # Restore the database mongo and insert data in mongodb
-mongorestore drop --host localhost --port 27017 --db videos --collection videos ../data/videos-raw/videos/videos.bson
+mongorestore drop --host localhost --port 27017 --db videos --collection videos /root/YouTubeContentManagement-DataEng/data/videos-raw/videos
