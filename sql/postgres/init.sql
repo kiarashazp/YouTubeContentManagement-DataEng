d977d00789c87b2ca90e63b21d3eb745668b1f06
Rational-Pigeon <y.nematzade.74@gmail.com>
2025-01-15 18:57:02 +0330

    modded init.sql and docker-compose

--- sql/postgres/init.sql
+++ sql/postgres/init.sql
@@ -1,5 +1,13 @@
-\connect channels_data;
+CREATE DATABASE IF NOT EXISTS airflow;
+CREATE DATABASE channels_data;
 
+CREATE USER airflow WITH PASSWORD 'airflow';
+CREATE USER admin WITH PASSWORD 'admin';
+
+GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
+GRANT ALL PRIVILEGES ON DATABASE channels_data TO admin;
+
+\connect channels_data;
 CREATE TABLE IF NOT EXISTS channels (
     id SERIAL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
