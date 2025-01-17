3b8f3b8a669d84b6e55c915023a4b479359cc460
Roozbeh <r_dargahi@yahoo.com>
2025-01-17 21:44:47 +0100

    Modified test_read_from_mongo_dag for testing MongoDB read task

--- workflow/dags/test_read_from_mongo_dag.py
+++ workflow/dags/test_read_from_mongo_dag.py
@@ -10,40 +10,50 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 # Task to read from MongoDB
 def read_from_mongo(**kwargs):
     try:
         # You can adjust the batch size in the Airflow UI without changing the code
-        batch_size = int(Variable.get("mongo_batch_size", default_var=1000))
+        batch_size = int(Variable.get("mongo_batch_size", default_var=10))
         mongo_hook = MongoHook(conn_id='MONGO_CONN_ID')
         client = mongo_hook.get_conn()
         db = client['videos']
         collection = db['videos']
         cursor = collection.find().batch_size(batch_size)
 
-        batches = []
+        batch_number = 0
         while cursor.alive:
             batch = []
-            for _ in range(batch_size):
+            for i in range(batch_size):
                 try:
                     doc = cursor.next()
+                    doc['_id'] = str(doc['_id'])
                     batch.append(doc)
                 except StopIteration:
                     break
             if batch:
-                batches.append(batch)
+                key = f'mongo_batch_{batch_number}'
+                kwargs['ti'].xcom_push(key=key, value=batch)
+                logger.info(f"Batch {batch_number} retrieved from MongoDB: {batch}")
+                batch_number += 1
 
-        kwargs['ti'].xcom_push(key='mongo_batches', value=batches)
+        logger.info(f"Total batches pushed to XCom: {batch_number}")
 
     except Exception as error:
         logger.error(f"Error extracting data from MongoDB: {error}")
         raise
+
+    finally:
+        client.close()
+
 
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2023, 1, 1),
     'retries': 1,
 }
+
 
 with DAG('test_read_from_mongo_dag', default_args=default_args, schedule_interval=None, catchup=False) as dag:
     read_task = PythonOperator(
