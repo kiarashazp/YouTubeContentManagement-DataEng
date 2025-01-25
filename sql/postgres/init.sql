9c326652bfc21961ccea142a5ba8d2cbe006d326
Rational-Pigeon <y.nematzade.74@gmail.com>
2025-01-25 22:09:54 +0000

    created indexes on username and created_at::date also username is not unique anymore => we can have multiple entries for channels.

--- sql/postgres/init.sql
+++ sql/postgres/init.sql
@@ -8,7 +8,7 @@ GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
 \connect channels_data;
 CREATE TABLE IF NOT EXISTS channels (
     id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE NOT NULL,
+    username TEXT NOT NULL,
     total_video_visit BIGINT CHECK (total_video_visit >= 0),
     video_count INT CHECK (video_count >= 0),
     start_date_timestamp BIGINT CHECK (start_date_timestamp >= 0),
@@ -17,5 +17,8 @@ CREATE TABLE IF NOT EXISTS channels (
     created_at TIMESTAMP DEFAULT '2025-01-01 00:00:00',
     update_count INT
 );
+
+CREATE INDEX idx_channels_username ON channels (username);
+CREATE INDEX idx_channels_created_date ON channels ((created_at::DATE));
 
 \COPY channels (username, total_video_visit, video_count, start_date_timestamp, followers_count, country, update_count) FROM '/docker-entrypoint-initdb.d/raw-channel-data/channels_stripped.csv' WITH (FORMAT csv, HEADER, DELIMITER ',', QUOTE '"', ESCAPE '"');
