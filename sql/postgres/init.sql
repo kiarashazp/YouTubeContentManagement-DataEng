13df1c7aa74d00bf9fb3a9aaa6480672ae445c85
Rational-Pigeon <y.nematzade.74@gmail.com>
2025-01-25 15:43:28 +0000

    implementing default timestamp for created_at

--- sql/postgres/init.sql
+++ sql/postgres/init.sql
@@ -14,8 +14,8 @@ CREATE TABLE IF NOT EXISTS channels (
     start_date_timestamp BIGINT CHECK (start_date_timestamp >= 0),
     followers_count BIGINT CHECK (followers_count >= 0),
     country TEXT,
-    created_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT '2025-01-01 00:00:00',
     update_count INT
 );
 
-\COPY channels (username, total_video_visit, video_count, start_date_timestamp, followers_count, country, created_at, update_count) FROM '/docker-entrypoint-initdb.d/raw-channel-data/channels_stripped.csv' WITH (FORMAT csv, HEADER, DELIMITER ',', QUOTE '"', ESCAPE '"');
+\COPY channels (username, total_video_visit, video_count, start_date_timestamp, followers_count, country, update_count) FROM '/docker-entrypoint-initdb.d/raw-channel-data/channels_stripped.csv' WITH (FORMAT csv, HEADER, DELIMITER ',', QUOTE '"', ESCAPE '"');
