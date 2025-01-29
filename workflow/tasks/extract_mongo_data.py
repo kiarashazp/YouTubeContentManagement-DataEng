ee3e0269a490b16ce9367f17c6cc0922f97c800f
kiarash <kiarashazp78@gmail.com>
2025-01-29 17:53:48 +0330

    convert start-end date

--- workflow/tasks/extract_mongo_data.py
+++ workflow/tasks/extract_mongo_data.py
@@ -22,8 +22,10 @@ def extract_mongo_data(**kwargs):
         logical_date = DagRun.execution_date  # Get the logical date (execution date) for the current run
         # logical_date = kwargs['logical_date']
         start_date = logical_date  # Start of the interval
-        end_date = logical_date + timedelta(days=1)  # End of the interval
+        start_date = start_date.strftime('%Y-%m-%dT%H:%M:%S.%f')
 
+        end_date = logical_date + timedelta(days=1)  # End of the interval
+        end_date = end_date.strftime('%Y-%m-%dT%H:%M:%S.%f')
         logger.info(f"Extracting data for date range: {start_date} to {end_date}")
 
         # Connect to MongoDB
