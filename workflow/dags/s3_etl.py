5309aa185a66e5bffea4cdad323f379c2669bac0
kiarash <kiarashazp78@gmail.com>
2025-01-29 19:38:33 +0330

    convert to function

--- workflow/dags/s3_etl.py
+++ workflow/dags/s3_etl.py
@@ -3,6 +3,7 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 import os
 import logging
+from airflow.decorators import dag
 
 from tasks.extract_json_data import extract_json_data
 from tasks.transform_json_data import transform_json_data
@@ -27,48 +28,56 @@ DAG_ID = os.path.basename(__file__).replace(".py", "")
 default_args = {
     'owner': 'airflow',
     'start_date': start_date,  # Start date = 5 days ago
-    'retries': 2,
+    'retries': 1,
     'retry_delay': timedelta(minutes=5),
     'on_failure_callback': notify_on_failure,
     'on_success_callback': notify_on_success,
     'on_retry_callback': notify_on_retry,
 }
 
-dag = DAG(
-    dag_id=DAG_ID,
-    default_args=default_args,
-    description='DAG for incremental processing and backfilling',
-    tags=["ETL", "Incremental Processing", "Backfilling"],
-    schedule_interval='0 19 * * *',  # Daily at 7 PM
-    catchup=True,  # Enable backfilling
-)
+# dag = DAG(
+#     dag_id=DAG_ID,
+#     default_args=default_args,
+#     description='DAG for incremental processing and backfilling',
+#     tags=["ETL", "Incremental Processing", "Backfilling"],
+#     schedule_interval='0 19 * * *',  # Daily at 7 PM
+#     catchup=True,  # Enable backfilling
+# )
 
 
 # --------------------- Define tasks
+
+@dag(dag_id=DAG_ID, default_args=default_args, description='DAG for incremental processing and backfilling', schedule_interval='0 19 * * *', tags=["ETL", "Incremental Processing", "Backfilling"], catchup=True)
+def s3_etl():
+    json_data_list = extract_json_data()
+    transformed_json_data = transform_json_data(json_data_list)
+    load_to_mongodb(transformed_json_data)
+
+
 # ------- ETL json from s3
-extract_json_data_from_s3 = PythonOperator(
-    task_id='extract_json_data',
-    python_callable=extract_json_data,
-    provide_context=True,
-    dag=dag
-)
-
-transform_json_data_from_s3 = PythonOperator(
-    task_id='transform_json_data',
-    python_callable=transform_json_data,
-    # op_args=[extract_json_data.output],
-    provide_context=True,
-    dag=dag
-)
-
-load_json_to_mongo_task = PythonOperator(
-    task_id='load_json_data',
-    python_callable=load_to_mongodb,
-    # op_args=[transform_json_data.output],
-    op_kwargs={'db_name': 'videos', 'collection_name': 'videos'},
-    provide_context=True,
-    dag=dag
-)
+# extract_json_data_from_s3 = PythonOperator(
+#     task_id='extract_json_data',
+#     python_callable=extract_json_data,
+#     provide_context=True,
+#     dag=dag
+# )
+#
+# transform_json_data_from_s3 = PythonOperator(
+#     task_id='transform_json_data',
+#     python_callable=transform_json_data,
+#     op_args=[extract_json_data],
+#     provide_context=True,
+#     dag=dag
+# )
+#
+# load_json_to_mongo_task = PythonOperator(
+#     task_id='load_json_data',
+#     python_callable=load_to_mongodb,
+#     op_args=[transform_json_data],
+#     op_kwargs={'db_name': 'videos', 'collection_name': 'videos'},
+#     provide_context=True,
+#     dag=dag
+# )
 
 # ------- ETL mongo to clickhouse
 # extract_mongo_task = PythonOperator(
@@ -97,10 +106,10 @@ load_json_to_mongo_task = PythonOperator(
 # Define task dependencies
 # extract_json_data_from_s3 >> transform_json_data_from_s3 >> load_json_to_mongo_task
 # extract_mongo_task >> transform_mongo_task >> load_mongo_task
-
-json_data_list = extract_json_data()
-transformed_json_data = transform_json_data(json_data_list)
-load_to_mongodb(transformed_json_data)
+#
+# json_data_list = extract_json_data()
+# transformed_json_data = transform_json_data(json_data_list)
+# load_to_mongodb(transformed_json_data)
 
 # mongo_data = extract_mongo_data()
 # transformed_mongo_data = transform_mongo_data(mongo_data)
