114ac5a829f894cab0ba7bb24d02ffaedd968f33
kiarash <kiarashazp78@gmail.com>
2025-01-30 21:46:13 +0330

    insert in mongo

--- workflow/tasks/extract_json_data.py
+++ workflow/tasks/extract_json_data.py
@@ -4,6 +4,7 @@ import tempfile
 
 from datetime import timedelta, datetime
 from airflow.models.dagrun import DagRun
+from airflow.providers.mongo.hooks.mongo import MongoHook
 
 from utils import connect_to_s3
 
@@ -84,27 +85,25 @@ def extract_from_s3_and_transform_data(**kwargs):
         logger.warning("No files found in the S3 bucket.")
         return []
 
-    with open("transformed_data.json", mode='w') as transformed_file:
-        logger.info(f"Transformed data written to temporary file: {transformed_file}")
     # Process each file in the S3 bucket
-        for file in response['Contents']:
-            if not file['Key'].endswith(".json"):
-                continue
+    for file in response['Contents']:
+        if not file['Key'].endswith(".json"):
+            continue
 
-            logger.info(f"Processing JSON file: {file['Key']}")
-            obj = s3_resource.get_object(Bucket=bucket_name, Key=file['Key'])
-            content = obj['Body'].read().decode('utf-8')
+        logger.info(f"Processing JSON file: {file['Key']}")
+        obj = s3_resource.get_object(Bucket=bucket_name, Key=file['Key'])
+        content = obj['Body'].read().decode('utf-8')
 
-            for line in content.splitlines():
-                try:
-                    json_data = json.loads(line)
-                    logger.info(f"load json files")
-                    transformed_json = transforming_data(json_data)
-                    logger.info(f"transformed data")
-                    json.dump(transformed_json, transformed_file)
-                    logger.info(f"insert in file ")
-                except json.JSONDecodeError as jde:
-                    logger.error(f"Error decoding JSON content in file {file['Key']}: {jde}")
+        for line in content.splitlines():
+            try:
+                json_data = json.loads(line)
+                logger.info(f"load json files")
+                transformed_json = transforming_data(json_data)
+                logger.info(f"transformed data")
+                connect_to_mongo(transformed_json)
+                logger.info(f"insert in file ")
+            except json.JSONDecodeError as jde:
+                logger.error(f"Error decoding JSON content in file {file['Key']}: {jde}")
 
 
 def transforming_data(input_data):
@@ -143,3 +142,34 @@ def transforming_data(input_data):
         logger.error(f"Error transforming document with _id {input_data.get('_id')} in ETL s3 to MongoDB: {ve}")
 
 
+def connect_to_mongo(**kwargs):
+    db_name = kwargs.get('db_name', 'videos')
+    collection_name = kwargs.get('collection_name', 'videos')
+
+    mongo_hook = MongoHook(conn_id='MONGO_CONN_ID')
+    client = mongo_hook.get_conn()
+
+    db = client[db_name]
+    collection = db[collection_name]
+
+    return client, collection
+
+
+def load_to_mongodb(data, **kwargs) -> None:
+    """
+    Loads transformed data from a file into MongoDB.
+
+    Args:
+        data (json): a transformed data for insert in mongodb.
+        **kwargs: Additional parameters including 'db_name' and 'collection_name'.
+    """
+    client, collection = connect_to_mongo(**kwargs)
+
+    try:
+        # Insert transformed data into MongoDB
+        collection.insert_one(data)
+        logger.info(f"Successfully inserted {data} documents into MongoDB")
+    except Exception as ve:
+        logger.error(f"Error inserting many documents in MongoDB: {ve}")
+    finally:
+        client.close()
