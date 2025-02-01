e12e890e4af9a991a22a1ec853268525ee97b096
Mohsen Zahmatkesh <msn.zmt81@gmail.com>
2025-02-01 20:19:40 +0330

    incrementally load data by last processed id

--- workflow/tasks/pg_to_clickhouse.py
+++ workflow/tasks/pg_to_clickhouse.py
@@ -9,30 +9,12 @@ from datetime import datetime
 
 
 def transfer_data_in_batches(**context):
+
+    batch_size = int(Variable.get("BATCH_SIZE_POSTGRES", default_var=10000))
     
-    # logical_date = DagRun.execution_date  # Get the logical date (execution date) for the current run
-    # ti = context['ti']
-    # start_date = "2025-01-27"  # Start of the interval
-    # end_date = "2025-01-29"
-    today = datetime.today().strftime('%Y-%m-%d')
-
-    batch_size = int(Variable.get("BATCH_SIZE_POSTGRES", default_var=1000))
-
-    pg_hook = PostgresHook(postgres_conn_id='postgres_default')
-
-    total_rows_query = "SELECT COUNT(*) FROM channels;"
-    total_rows = pg_hook.get_first(total_rows_query)[0]
-
-    offset = 0
-    
-    # query_file_path = Variable.get("init_clickhouse_query_path")
     query_file_path = "utils/clickhouse_schema_channels.sql"
     query = load_query_from_file(query_file_path)
 
-    # clickhouse_host = Variable.get("CLICKHOUSE_HOST")
-    # clickhouse_user = Variable.get("CLICKHOUSE_USER")
-    # clickhouse_pass = Variable.get("CLICKHOUSE_PASS")
-    # clickhouse_db = Variable.get("CLICKHOUSE_DB")
 
     client = Client(
         host='clickhouse',
@@ -41,12 +23,11 @@ def transfer_data_in_batches(**context):
         password='airflow',
         database='bronze'
     )
-    
     client.execute(query)
     
-    while offset < total_rows:
-        extract_postgres_batch(today, batch_size, offset, **context)
-
+    while True:
+        
+        run=extract_postgres_batch(batch_size, **context)
+        if not run :
+            break
         load_clickhouse_batch(**context)
-
-        offset += batch_size
--- workflow/utils/extract_postgres.sql
+++ workflow/utils/extract_postgres.sql
@@ -3,6 +3,6 @@ SELECT
     start_date_timestamp, followers_count, country, 
     created_at, update_count 
 FROM channels
-WHERE created_at::DATE = %s
+WHERE id > %s
 ORDER BY id
-LIMIT %s OFFSET %s;
+LIMIT %s;
