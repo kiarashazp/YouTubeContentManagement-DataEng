f8439a7b37292523dfacc7ab7b267d3b1e611bcd
kiarash <kiarashazp78@gmail.com>
2025-02-01 19:56:44 +0330

    created at etl_mongo_to_clickhouse

--- workflow/tasks/etl_mongo_to_clickhouse.py
+++ workflow/tasks/etl_mongo_to_clickhouse.py
@@ -64,7 +64,6 @@ def etl_mongo_to_clickhouse(**kwargs):
                 logger.info(f"Transforming batch of {len(batch_data)} documents")
                 transformed_batch = []
                 for doc in batch_data:
-                    created_at = datetime.strptime(doc.get('created_at', '1970-01-01T00:00:00'), '%Y-%m-%dT%H:%M:%S')
                     videos_values = {
                         'id': str(doc.get('id', '')),
                         'owner_username': doc.get('owner_username', ''),
@@ -78,7 +77,7 @@ def etl_mongo_to_clickhouse(**kwargs):
                         'comments': doc.get('comments', ''),
                         'like_count': doc.get('like_count', 0),
                         'is_deleted': doc.get('is_deleted', False),
-                        'created_at': created_at,
+                        'created_at': doc.get('created_at', datetime(1970, month=1, day=1)),
                         'expire_at': doc.get('expire_at', 0),
                         'update_count': doc.get('update_count', 0)
                     }
